13642c054a62d48992893a4e48d2befe436d09f8
Matt North <matt@cryptechconsulting.co.uk>
2015-07-29 08:52:37 +0100

    Bootstrap updates
    Removing Puppet code - not required for this.

--- ELK/bootstrap_server.sh
+++ ELK/bootstrap_server.sh
@@ -1,17 +1,6 @@
         echo Installing.
         date > /etc/systembuild
         
-	# Install and configure puppet server
-
-	rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-        yum clean all
-        yum -y install puppet-server sleep
-        sed -i -e '/vardir\/ssl/a dns_alt_names = puppet,puppet-server,puppet-server.localdomain' /etc/puppet/puppet.conf
-        puppet master --verbose --no-daemonize &
-        sleep 30
-        sed -i -e 's/=permissive/=disabled/' /etc/selinux/config
-        chkconfig puppetmaster on
-        
 	# Java & elasticsearch install
 
 	yum -y install java-1.7.0-openjdk
